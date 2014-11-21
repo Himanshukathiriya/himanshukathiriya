26c77e3ff3ea4818358a5ce03404f89942791f6b
Himanshukathiriya <Himanshukathiriya@users.noreply.github.com>
2014-11-21 14:53:34 +0530

    Update default.aspx.cs

--- default.aspx.cs
+++ default.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 
 
-public partial class Default2 : System.Web.UI.Page
+public partial class Default : System.Web.UI.Page
 {
     protected void Button1_click(object sender, EventArgs e)
     
